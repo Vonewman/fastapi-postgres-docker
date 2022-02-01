4956d95f8d31f8500a012c9a2dbb210bdfb80296
vonewman <vonewman7@gmail.com>
2022-02-01 14:08:27 +0000

    the finale code

---
+++ models.py
@@ -0,0 +1,20 @@
+import datetime as _dt
+import sqlalchemy as _sql
+
+import database as _database
+
+class Contact(_database.Base):
+    """Somme Contacts
+    
+    Args:
+        _database ([type]): [description]
+    """
+    __tablename__ = "contacts"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    first_name = _sql.Column(_sql.String, index=True)
+    last_name = _sql.Column(_sql.String, index=True)
+    email = _sql.Column(_sql.String, index=True, unique=True)
+    phone_number = _sql.Column(_sql.String, index=True, unique=True)
+    date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+    
+    
